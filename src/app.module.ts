a81311b8da5d7d39500cf27d02558e27ed1e186b
tungba103 <tungbt.work@protonmail.com>
2024-10-17 17:46:09 +0700

    remove sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
